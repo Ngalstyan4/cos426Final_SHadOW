2362a675d1e97288a07ce72cfa107e315939de3b
LucasS98 <lucasfernansalvador@outlook.com>
2019-05-10 18:44:47 -0400

    BOXES!
    MAKE BOXES!

--- js/events.js
+++ js/events.js
@@ -1,11 +1,15 @@
 "use strict";
 
-var mouse = new THREE.Vector2(200000,2000000), INTERSECTED
+var mouse = new THREE.Vector2(200000,2000000), INTERSECTED;
+
+var click = false;
+var clickMouse = new THREE.Vector2(200000,2000000);
 
 function initEventHandlers() {
     document.addEventListener( 'mousemove', onDocumentMouseMove, false );
     window.addEventListener("resize", onWindowResize, false);
 
+    window.addEventListener("mousedown", onDocumentMouseDown, false);
 }
 
 // event handlers
@@ -13,6 +17,13 @@ function onDocumentMouseMove( event ) {
     event.preventDefault();
     mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
     mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+}
+
+function onDocumentMouseDown( event ) {
+    event.preventDefault();
+    clickMouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    clickMouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    click = true;
 }
 
 function onWindowResize() {
--- js/render.js
+++ js/render.js
@@ -2,6 +2,8 @@
 
 if (!Detector.webgl) Detector.addGetWebGLMessage();
 
+var meshes = [];
+
  //all below defined in initWorld BEGIN
  // var stats;
  // var camera;
@@ -15,7 +17,7 @@ function render() {
     // option to auto-rotate camera
     // if (rotate) {
     //     let cameraRadius = Math.sqrt(
-    //         camera.position.x * camera.position.x + camera.position.z * camera.position.z
+    //         camera.position.x * camera.position.x + camera.position.z * camera.position.z;
     //     );
     //     camera.position.x = Math.cos(timer) * cameraRadius;
     //     camera.position.z = Math.sin(timer) * cameraRadius;
@@ -23,15 +25,90 @@ function render() {
     camera.lookAt(scene.position);
     renderer.render(scene, camera); // render the scene
 
+    let cellSideLen = CONFIG.PLAYGROUND.size /CONFIG.PLAYGROUND.divisions;
+    var intersectArray = meshes.slice();
+    intersectArray.push(wholePlane);
+
+    if (click){
+        raycaster.setFromCamera( clickMouse, camera );
+        var intersects = raycaster.intersectObjects( intersectArray );
+        if ( intersects.length > 0 ) {
+            let {x,y,z} = intersects[0].point;
+            x = Math.sign(x) * (Math.abs(x) - Math.abs(x) % cellSideLen+cellSideLen/2);
+            y = (Math.abs(y) - Math.abs(y) % cellSideLen+cellSideLen/2);
+            z = Math.sign(z) * (Math.abs(z) - Math.abs(z) % cellSideLen+cellSideLen/2);
+            
+            var geometry = new THREE.BoxGeometry( cellSideLen, cellSideLen, cellSideLen );
+            var material = new THREE.MeshBasicMaterial( {color: 0x4286f4} );
+            var cube = new THREE.Mesh( geometry, material );
+            scene.add( cube );
+
+            meshes.push(cube);
+
+            cube.position.set(x,y,z);
+        }
+        click = false;
+    }
 
     /* Render mouse hover effects BEGIN */
     raycaster.setFromCamera( mouse, camera );
-    var intersects = raycaster.intersectObjects( [wholePlane] );
+    var intersects = raycaster.intersectObjects( intersectArray );
     if ( intersects.length > 0 ) {
-        let {x,z} = intersects[0].point;
-        let cellSideLen = CONFIG.PLAYGROUND.size /CONFIG.PLAYGROUND.divisions;
-        plane.position.x =  Math.sign(x) * (Math.abs(x) - Math.abs(x) % cellSideLen+cellSideLen/2);
-        plane.position.z =  Math.sign(z) * (Math.abs(z) - Math.abs(z) % cellSideLen+cellSideLen/2);
+        let {x,y,z} = intersects[0].point;
+
+        if (intersects[0].object !== wholePlane){
+            var normal = intersects[0].face.normal.normalize();        
+            if (normal.equals(new THREE.Vector3(0,1,0)) || normal.equals(new THREE.Vector3(0,-1,0))){
+                plane.rotation.x = 3.14/2;
+                plane.rotation.y = 0;
+                plane.rotation.z = 0;
+
+                
+                plane.position.x =  Math.sign(x) * (Math.abs(x) - Math.abs(x) % cellSideLen+cellSideLen/2);
+
+                if (normal.equals(new THREE.Vector3(0,1,0)))
+                    plane.position.y =  y + 1;
+                else
+                    plane.position.y =  y - 1;
+
+                plane.position.z =  Math.sign(z) * (Math.abs(z) - Math.abs(z) % cellSideLen+cellSideLen/2);
+            }
+            else if (normal.equals(new THREE.Vector3(1,0,0)) || normal.equals(new THREE.Vector3(-1,0,0))){
+                plane.rotation.x = 0;
+                plane.rotation.y = 3.14/2;
+                plane.rotation.z = 3.14/2;
+
+                if (normal.equals(new THREE.Vector3(1,0,0)))
+                    plane.position.x =  x + 1;
+                else
+                    plane.position.x =  x - 1;
+                
+                plane.position.y =  Math.sign(y) * (Math.abs(y) - Math.abs(y) % cellSideLen +cellSideLen/2);
+                plane.position.z =  Math.sign(z) * (Math.abs(z) - Math.abs(z) % cellSideLen+cellSideLen/2);
+            }
+            else if (normal.equals(new THREE.Vector3(0,0,1)) || normal.equals(new THREE.Vector3(0,0,-1))){
+                plane.rotation.x = 0;
+                plane.rotation.y = 0;
+                plane.rotation.z = 0;
+
+                plane.position.x =  Math.sign(x) * (Math.abs(x) - Math.abs(x) % cellSideLen+cellSideLen/2);
+                plane.position.y =  Math.sign(y) * (Math.abs(y) - Math.abs(y) % cellSideLen + cellSideLen/2);
+                
+                if (normal.equals(new THREE.Vector3(0,0,1)))
+                    plane.position.z =  z + 1;
+                else
+                    plane.position.z =  z - 1;
+            }
+        }
+        else{
+            plane.rotation.x = 3.14/2;
+            plane.rotation.y = 0;
+            plane.rotation.z = 0;
+
+            plane.position.x =  Math.sign(x) * (Math.abs(x) - Math.abs(x) % cellSideLen+cellSideLen/2);
+            plane.position.y =  y + 1;
+            plane.position.z =  Math.sign(z) * (Math.abs(z) - Math.abs(z) % cellSideLen+cellSideLen/2);
+        }
     }
     /* Render mouse hover effects END */
 
